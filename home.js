d82dca9294457f32718c1b6b0517dc571e351211
XCode-Correa <yaboize355@gmail.com>
2026-06-15 09:20:24 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,96 +1,92 @@
-const slider = document.querySelector('.slider');
 
-if (slider) {
+let active = 0;
 
-    let active = 0;
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let slideTitle = document.getElementById('slideTitle');
+let slideDesc = document.getElementById('slideDesc');
+let quoteBtn = document.getElementById('quoteBtn');
 
-    let list = document.querySelector('.slider .list');
-    let items = document.querySelectorAll('.slider .list .item');
-    let dots = document.querySelectorAll('.slider .dots li');
-    let prev = document.getElementById('prev');
-    let next = document.getElementById('next');
-    let slideTitle = document.getElementById('slideTitle');
-    let slideDesc = document.getElementById('slideDesc');
-    let quoteBtn = document.getElementById('quoteBtn');
+let lengthItems = items.length - 1;
 
-    let lengthItems = items.length - 1;
+next.onclick = function () {
+    if (active + 1 > lengthItems) {
+        active = 0;
+    } else {
+        active++;
+    }
+    reloadSlider();
+};
 
-    next.onclick = function () {
-        if (active + 1 > lengthItems) {
-            active = 0;
-        } else {
-            active++;
-        }
-        reloadSlider();
-    };
+prev.onclick = function () {
+    if (active - 1 < 0) {
+        active = lengthItems;
+    } else {
+        active--;
+    }
+    reloadSlider();
+};
 
-    prev.onclick = function () {
-        if (active - 1 < 0) {
-            active = lengthItems;
-        } else {
-            active--;
-        }
-        reloadSlider();
-    };
+let refreshSlider = setInterval(() => {
+    next.click();
+}, 5000);
 
-    let refreshSlider = setInterval(() => {
-        next.click();
-    }, 5000);
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
 
-    function reloadSlider() {
-        let checkLeft = items[active].offsetLeft;
-        list.style.left = -checkLeft + 'px';
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
 
-        let lastActiveDot = document.querySelector('.slider .dots li.active');
-        lastActiveDot.classList.remove('active');
-        dots[active].classList.add('active');
+    slideTitle.textContent = slideContent[active].title;
+    slideDesc.textContent = slideContent[active].desc;
 
-        slideTitle.textContent = slideContent[active].title;
-        slideDesc.textContent = slideContent[active].desc;
-
-        if (active === 4) {
-            quoteBtn.classList.add("show");
-        } else {
-            quoteBtn.classList.remove("show");
-        }
-
-        document.querySelector('.slider-text').classList.remove('fade-text');
-        void document.querySelector('.slider-text').offsetWidth;
-        document.querySelector('.slider-text').classList.add('fade-text');
-
-        clearInterval(refreshSlider);
-        refreshSlider = setInterval(() => {
-            next.click();
-        }, 5000);
+    if (active === 4) {
+        quoteBtn.classList.add("show");
+    } else {
+        quoteBtn.classList.remove("show");
     }
 
-    dots.forEach((li, key) => {
-        li.addEventListener('click', function () {
-            active = key;
-            reloadSlider();
-        });
-    });
+    document.querySelector('.slider-text').classList.remove('fade-text');
+    void document.querySelector('.slider-text').offsetWidth;
+    document.querySelector('.slider-text').classList.add('fade-text');
 
-    const slideContent = [
-        {
-            title: "C&M PARTNERS",
-            desc: "Dream, Design, Develop"
-        },
-        {
-            title: "DREAM",
-            desc: "Turn your dream home into reality."
-        },
-        {
-            title: "DESIGN",
-            desc: "Design your ideas."
-        },
-        {
-            title: "DEVELOP",
-            desc: "Develop your perspective."
-        },
-        {
-            title: "A New Foundation",
-            desc: "What you build today, will shape tomorrow."
-        }
-    ];
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => {
+        next.click();
+    }, 5000);
 }
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+    });
+});
+
+const slideContent = [
+    {
+        title: "C&M PARTNERS",
+        desc: "Dream, Design, Develop"
+    },
+    {
+        title: "DREAM",
+        desc: "Turn your dream home into reality."
+    },
+    {
+        title: "DESIGN",
+        desc: "Design your ideas."
+    },
+    {
+        title: "DEVELOP",
+        desc: "Develop your perspective."
+    },
+    {
+        title: "A New Foundation",
+        desc: "What you build today, will shape tomorrow."
+    }
+];
